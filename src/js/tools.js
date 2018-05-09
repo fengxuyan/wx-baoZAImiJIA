5ea169bf7dadfc48931432fe5705a27a6053178b
2933992579@qq.com <2933992579@qq.com>
2018-05-09 18:03:30 +0800

    新增分类

---
+++ src/js/tools.js
@@ -0,0 +1,18 @@
+
+var body_height = document.documentElement.clientHeight;
+$('body').css('height', body_height);
+$('.sort-container').css('height', body_height);
+function serilizeURL(url) {
+    var rs = url.split("?")[1];
+    var arr = rs.split("&");
+    var json = {};
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf("=") != -1) {
+            json[arr[i].split("=")[0]] = arr[i].split("=")[1];
+        }
+        else {
+            json[arr[i]] = "undefined";
+        }
+    }
+    return json;
+}
